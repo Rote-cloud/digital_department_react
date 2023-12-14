2879e608496f2df0a59d558aa3c13d7e6e6c5df3
Кирилл <pote1357@mail.ru>
2023-12-14 20:54:16 +0300

    Добавил 2 большие картинки с описанием

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import TopBar from './topBar.tsx'
 import VirtualHealthcare from './virtualHealthcare.tsx'
 import OurServices from './ourServices.tsx'
+import OurMobile from './ourMobile.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TopBar />
     <VirtualHealthcare />
     <OurServices />
+    <OurMobile />
   </React.StrictMode>,
 )
